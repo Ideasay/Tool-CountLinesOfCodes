b498d749f52309e54a9f2bcf37b56e8b5d674821
朕是太上皇 <31928726+Ideasay@users.noreply.github.com>
2018-03-29 21:49:20 +0800

    Update count_lines_of_codes.py

--- count_lines_of_codes.py
+++ count_lines_of_codes.py
@@ -1,87 +1,80 @@
 import os
 
-dir = input("enter the folder path:")
-#输入路径
+#获取一个文件的后缀名
+def getend(file):
+    return os.path.splitext(file)[1]
 
+#统计c,verilog,py,sh四个文件的有效行数的函数
+## countc
+def countc(file):
+    fhand = open(os.path.join(file),encoding = 'ISO-8859-1')
+    nums = 0
+    for line in fhand:
+        line = line.strip()
+        if line.startswith('//') or line.startswith('/*') or not len(line):
+            continue
+        nums = nums + 1
+    return nums
+
+##countpy
+def countpy(file):
+    fhand = open(os.path.join(file),encoding = 'utf-8')
+    nums = 0
+    for line in fhand:
+        line = line.strip()
+        if line.startswith('#') or not len(line):
+            continue
+        nums = nums + 1
+    return nums
+
+##countv
+def countv(file):
+    fhand = open(os.path.join(file),encoding = 'utf-8')
+    nums = 0
+    flag = 1
+    for line in fhand:
+        line = line.strip()
+        if line.startswith('//') or line.startswith('/*') or not len(line):
+            continue
+        if flag == 1:
+            if line.startswith('module'):
+                flag = 0
+            else:
+                print('this is not  a verilog codes file')
+                break
+        nums = nums + 1
+    return nums
+
+##countsh
+def countsh(file):
+    fhand = open(os.path.join(file),encoding = 'ISO-8859-1')
+    nums = 0
+    for line in fhand:
+        line = line.strip()
+        if line.startswith('#') or not len(line):
+            continue
+        nums = nums + 1
+    return nums
+
+#定义一个后缀对应处理方式的字典
+type2func =  {'.c':countc,'.py':countpy,'.v':countv,'.sh':countsh}
+
+
+#主函数
+
+##输入路径
+dir = input("enter the folder path:")
+
+##判断路径是否存在
 if not os.path.exists(dir):
     print('this dir is not existent')
-#判断路径是否存在
-
-a = 0
-b = 0
-c = 0
-d = 0
-
-
-for root,dirs,files in os.walk(dir):
-    for file in files:
-        if  os.path.splitext(file)[1] == '.py':
-            a = 1
-        if  os.path.splitext(file)[1] == '.cpp':
-            b = 1
-        if  os.path.splitext(file)[1] == '.c':
-            c = 1
-        if  os.path.splitext(file)[1] == '.JAVA':
-            d = 1
-total = a + b + c + d
-#统计文件夹内的编程语言数目
-        
-name = list() 
-number = list()
-
-for i in range(total):
-    name.append([])
-    number.append([])
-#创建一个多维列表
-
-for root,dirs,files in os.walk(dir):
-    for file in files:
-        if  os.path.splitext(file)[1] == '.py':
-            name[a - 1].append(str(os.path.join(file)))
-            fhand = open(os.path.join(root,file),encoding = 'utf-8')
-            nums_0 = 0
-            for line in fhand:
-                line = line.strip()
-                if line.startswith('#') or not len(line):
-                    continue
-                nums_0 = nums_0 + 1
-            number[a - 1].append(nums_0)
-        if  os.path.splitext(file)[1] == '.c':
-            name[a + b  - 1].append(str(os.path.join(file)))
-            fhand = open(os.path.join(root,file),encoding = 'ISO-8859-1')
-            nums_1 = 0
-            for line in fhand:
-                line = line.strip()
-                if line.startswith('//') or line.startswith('/*') or not len(line):
-                    continue
-                nums_1 = nums_1 + 1
-            number[a + b  - 1].append(nums_1)
-        if  os.path.splitext(file)[1] == '.cpp':
-            name[a + b + c  - 1].append(str(os.path.join(file)))
-            fhand = open(os.path.join(root,file),encoding = 'ISO-8859-1')
-            nums_2 = 0
-            for line in fhand:
-                line = line.strip()
-                if line.startswith('//') or line.startswith('/*') or not len(line):
-                    continue
-                nums_2 = nums_2 + 1
-            number[a + b + c  - 1].append(nums_2)
-        if  os.path.splitext(file)[1] == '.JAVA':
-            name[a + b + c + d  - 1].append(str(os.path.join(file)))
-            fhand = open(os.path.join(root,file),encoding = 'ISO-8859-1')
-            nums_3 = 0
-            for line in fhand:
-                line = line.strip()
-                if line.startswith('//') or line.startswith('/*') or not len(line):
-                    continue
-                nums_3 = nums_3 + 1
-            number[a + b + c + d  - 1].append(nums_3)
-#统计行数
-
-for i in range(total):
-    for j in range(len(name[i])):
-        print(name[i][j],':',number[i][j])
-#输出
-        
     
-input()
+##统计并输出
+for root,dirs,files in os.walk(dir):
+    for file in files:
+        countfile = type2func[getend(os.path.join(root,file))]
+        name_tmp = os.path.join(file)
+        nums_tmp = countfile(os.path.join(root,file))
+        print(name_tmp,':',nums_tmp)
+        
+        
